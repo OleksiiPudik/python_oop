c97c6b6ff0c5520483134e780758de6cb48906c9
OleksiiPudik <alekseypudik@gmail.com>
2026-06-29 22:27:16 +0300

    Section 3.8 (getitem, setitem, delitem), tasc 3 - Trask class

--- oop/oop-3_8.py
+++ oop/oop-3_8.py
@@ -1,22 +1,76 @@
-class Record:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+# tasc 2
+# class Record:
+#     def __init__(self, **kwargs):
+#         self.__dict__.update(kwargs)
     
-    def __getitem__(self, key):
-        if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.__dict__):
-            raise IndexError("неверный индекс поля")
+#     def __getitem__(self, key):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.__dict__):
+#             raise IndexError("неверный индекс поля")
         
-        lst_temp = list(self.__dict__.values())
+#         lst_temp = list(self.__dict__.values())
 
-        return lst_temp[key]
+#         return lst_temp[key]
     
-    def __setitem__(self, key, value):
-        if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.__dict__):
-            raise IndexError("неверный индекс поля")
+#     def __setitem__(self, key, value):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.__dict__):
+#             raise IndexError("неверный индекс поля")
         
-        lst_temp = list(self.__dict__.keys())
+#         lst_temp = list(self.__dict__.keys())
 
-        self.__dict__[lst_temp[key]] = value
+#         self.__dict__[lst_temp[key]] = value
 
-a = Record(aaa=111, bbb=222, ccc=333)
-print(a.bbb)
+# a = Record(aaa=111, bbb=222, ccc=333)
+# print(a.bbb)
+
+# ---------------------------------------------------------
+# tasc 3
+# class Track:
+#     def __init__(self, start_x, start_y):
+#         self.start_x = start_x
+#         self.start_y = start_y
+#         self.route = []
+    
+#     def add_point(self, x, y, speed):
+#         self.route.append([(x, y), speed])
+    
+#     def __getitem__(self, key):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.route):
+#             raise IndexError("некорректный индекс")
+        
+#         return self.route[key]
+    
+#     def __setitem__(self, key, value):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= len(self.route):
+#             raise IndexError("некорректный индекс")
+        
+#         self.route[key][1] = value
+
+# tr = Track(1, 2)
+# tr.add_point(3, 4, 10)
+# tr.add_point(13, 14, 20)
+# tr[1] = 30
+# c, s = tr[1]
+# print(s)
+
+# -----------------------------------------------------------
+# tasc 4
+class Array:
+    def __init__(self, max_length, cell):
+        self.cell = cell
+        self.arr = []
+
+
+class Integer:
+    def __init__(self, start_value):
+        self.__value = start_value
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, val):
+        if isinstance(val, bool) or not isinstance(val, int):
+            raise ValueError("должно быть целое число")
+        
+        self.__value = val
