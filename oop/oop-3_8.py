42d56b8596ed0fcec893b5193f31ca2edc077d14
OleksiiPudik <alekseypudik@gmail.com>
2026-07-04 14:17:32 +0300

    Section 3.8 (getitem, setitem, delitem), repeat descriptors, task 5 - TableValues, CellInteger, IntegerValue

--- oop/oop-3_8.py
+++ oop/oop-3_8.py
@@ -53,36 +53,81 @@
 # print(s)
 
 # -----------------------------------------------------------
-# tasc 4
-class Array:
-    def __init__(self, max_length, cell):
-        self.max_length = max_length
-        self.arr = [cell(0) for _ in range(max_length)]
+# task 4
+# class Array:
+#     def __init__(self, max_length, cell):
+#         self.max_length = max_length
+#         self.arr = [cell(0) for _ in range(max_length)]
+    
+#     def __getitem__(self, key):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= self.max_length:
+#             raise IndexError("неверный индекс для доступа к элементам массива")
+        
+#         return self.arr[key].value
+    
+#     def __setitem__(self, key, val):
+#         if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= self.max_length:
+#             raise IndexError("неверный индекс для доступа к элементам массива")
+        
+#         self.arr[key].value = val
+
+
+# class Integer:
+#     def __init__(self, start_value):
+#         self.value = start_value
+    
+#     @property
+#     def value(self):
+#         return self.__value
+    
+#     @value.setter
+#     def value(self, val):
+#         if isinstance(val, bool) or not isinstance(val, int):
+#             raise ValueError("должно быть целое число")
+        
+#         self.__value = val
+
+# ----------------------------------------------------------
+
+# task 5
+class IntegerValue:
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+    
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+    
+    def __set__(self, instance, value):
+        if isinstance(value, bool) or not isinstance(value, int):
+            raise ValueError("возможны только целочисленные значения")
+        
+        setattr(instance, self.name, value)
+
+
+class CellInteger:
+    value = IntegerValue()
+
+    def __init__(self, start_value=0):
+        self.value = start_value
+
+class TableValues:
+    def __init__(self, rows, cols, cell=None):
+        if cell is None:
+            raise ValueError("параметр cell не указан")
+        else:
+            self.cells = tuple(tuple(cell() for _ in range(cols)) for _ in range(rows))
     
     def __getitem__(self, key):
-        if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= self.max_length:
-            raise IndexError("неверный индекс для доступа к элементам массива")
-        
-        return self.arr[key].value
+        r, c = key
+        return self.cells[r][c].value
     
     def __setitem__(self, key, val):
-        if isinstance(key, bool) or not isinstance(key, int) or key < 0 or key >= self.max_length:
-            raise IndexError("неверный индекс для доступа к элементам массива")
-        
-        self.arr[key].value = val
+        r, c = key
+        self.cells[r][c].value = val
 
 
-class Integer:
-    def __init__(self, start_value):
-        self.value = start_value
-    
-    @property
-    def value(self):
-        return self.__value
-    
-    @value.setter
-    def value(self, val):
-        if isinstance(val, bool) or not isinstance(val, int):
-            raise ValueError("должно быть целое число")
+tb = TableValues(1, 2, CellInteger)
+tb[0, 0] = 3.5
+print(tb[0, 0])
         
-        self.__value = val
+        
