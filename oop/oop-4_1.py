972a666d169bc86c44da4fffc42ad60dd9c66e2c
OleksiiPudik <alekseypudik@gmail.com>
2026-08-06 22:19:38 +0300

    Раздел 4 Наследование и полиморфизм, продолжаю разбирать теорию

--- oop/oop-4_1.py
+++ oop/oop-4_1.py
@@ -1,17 +1,44 @@
-class Parent:
-    lst = [1, 2, 2]
+# class Parent:
+#     lst = [1, 2, 2]
 
 
-class Inheritor1(Parent):
-    pass
+# class Inheritor1(Parent):
+#     pass
 
 
-class Inheritor2(Parent):
-    pass
+# class Inheritor2(Parent):
+#     pass
 
 
-a = Inheritor1()
-b = Inheritor2()
-a.lst.append(99)
+# a = Inheritor1()
+# b = Inheritor2()
+# a.lst.append(99)
 
-print(Inheritor1.__mro__)
+# print(Inheritor1.__mro__)
+
+
+class Table:
+    def __init__(self, model, color):
+        self.model = model
+        self. color = color
+        
+
+
+class RoundTable(Table):
+    def __init__(self, model, color, radius, height):
+        super().__init__(model, color)
+        self.radius = radius
+        self.height = height
+        
+
+
+class SquareTable(Table):
+    def __init__(self, model, color, side, height):
+            super().__init__(model, color)
+            self.side = side
+            self.height = height
+            print(hash(self))
+
+
+rt = RoundTable("qqq", "red", 5, 10)
+rs = SquareTable("aaa", "green", 5, 10)
